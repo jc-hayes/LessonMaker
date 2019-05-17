1658d6a0f28cf08cb80ea0d28d5f423f8ed0a3e0
James H <jchayes86@gmail.com>
2019-05-17 11:45:21 -0700

    Added Lesson Model

--- LessonMaker/Models/Lesson.cs
+++ LessonMaker/Models/Lesson.cs
@@ -7,5 +7,15 @@ namespace LessonMaker.Models
 {
     public class Lesson
     {
+        private int Id;
+
+        private String Title;
+
+        private String Content;
+
+        private int Votes;
+
+        private DateTime CreationDate;
+
     }
 }
